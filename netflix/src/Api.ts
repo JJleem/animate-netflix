66769ea68db83435c8eccdbc9331158afd5989e9
leemjaejun <leemjaejun@gmail.com>
2024-05-30 14:30:16 +0900

    240530

--- netflix/src/Api.ts
+++ netflix/src/Api.ts
@@ -2,12 +2,27 @@ const API_KEY = "3fadc08fd94e2086a1fc3e574ca59870";
 
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+export interface IMovie {
+  id: number;
+  title: string;
+  overview: string;
+  backdrop_path: string;
+  poster_path: string;
+}
+
+export interface IGetmoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export const getMovies = () => {
-  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
-    (response) => response.json()
-  );
+  return fetch(
+    `${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=ko-kr&region=kr`
+  ).then((response) => response.json());
 };
-
-// https://api.themoviedb.org/3/movie/now_playing?api_key=3fadc08fd94e2086a1fc3e574ca59870
-
-// https://api.themoviedb.org/3/movie/now_playing?language=ko-kr&page=1&region=kr?api_key=3fadc08fd94e2086a1fc3e574ca59870
